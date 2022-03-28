8ca6c9dbe5e13d78767565c0e07d258da99c6725
hungtran.contact@gmail.com <imhererb@gmail.com>
2022-03-28 23:24:44 +0700

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -86,12 +86,13 @@ import { createClient } from '@supabase/supabase-js';
 
 // export default Home
 
-const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
-);
+
 
 export async function getStaticProps() {
+  const supabaseAdmin = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+  )
   const { data } = await supabaseAdmin.from('images').select('*');
   return {
     props: {
